19fd68c5a1e221c06ea99df211a284bf71056027
sjojon <90796535+sjojon@users.noreply.github.com>
2023-05-18 14:02:49 +0200

    Update Program.cs

--- Ukesoppgaveuke2/Program.cs
+++ Ukesoppgaveuke2/Program.cs
@@ -7,48 +7,57 @@
             {
                 //Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-                List<Student> studenter = new()
+                var studenter = Courses();
+
+                ShowCourses(studenter);
+            }
+        }
+
+        private static List<Student> Courses()
+        {
+            List<Student> studenter = new()
+            {
+                new Student("Heibert", 42, "Radiobilkjøring"),
+                new Student("Bård", 29, "Radiobilkjøring"),
+                new Student("Tine", 31, "Radiobilkjøring"),
+                new Student("Kim", 25, "Data"),
+                new Student("Hanne", 21, "Data"),
+                new Student("Tore", 35, "Data"),
+                new Student("Tom", 20, "Løping"),
+                new Student("Tim", 44, "Løping"),
+                new Student("Petter", 38, "Løping"),
+                new Student("Per", 49, "Koding"),
+                new Student("Kåre", 28, "Koding"),
+                new Student("Anne", 34, "Koding"),
+            };
+
+            List<string> kurs = new();
+            foreach (var student in studenter)
+            {
+                if (!kurs.Contains(student.Kurs))
                 {
-                    new Student("Heibert", 42, "Radiobilkjøring"),
-                    new Student("Bård", 29, "Radiobilkjøring"),
-                    new Student("Tine", 31, "Radiobilkjøring"),
-                    new Student("Kim", 25, "Data"),
-                    new Student("Hanne", 21, "Data"),
-                    new Student("Tore", 35, "Data"),
-                    new Student("Tom", 20, "Løping"),
-                    new Student("Tim", 44, "Løping"),
-                    new Student("Petter", 38, "Løping"),
-                    new Student("Per", 49, "Koding"),
-                    new Student("Kåre", 28, "Koding"),
-                    new Student("Anne", 34, "Koding"),
-                };
-
-                List<string> kurs = new();
-                foreach (var student in studenter)
-                {
-                    if (!kurs.Contains(student.Kurs))
-                    {
-                        kurs.Add(student.Kurs);
-                        Console.WriteLine(student.Kurs);
-                    }
-                }
-
-                //foreach (var student in studenter)
-                //{
-                //    Console.WriteLine($"{student.Kurs}");
-                //}
-
-                Console.WriteLine("Skriv inn et kurs: ");
-                string input = Console.ReadLine().ToLower();
-
-                foreach (var student in studenter)
-                {
-                    if (student.Kurs.ToLower() == input)
-                    {
-                        Console.WriteLine($"{student.Name}, {student.Age}, {student.Kurs}");
-                    }
+                    kurs.Add(student.Kurs);
+                    Console.WriteLine(student.Kurs);
                 }
             }
+
+            return studenter;
+        }
+
+        private static void ShowCourses(List<Student> studenter)
+        {
+            Console.WriteLine("Skriv inn et kurs: ");
+            string input = Console.ReadLine().ToLower();
+
+            foreach (var student in studenter)
+            {
+                if (student.Kurs.ToLower() == input)
+                {
+                    Console.WriteLine($"{student.Name}, {student.Age}, {student.Kurs}");
+                }
+            }
+
+            ShowCourses(studenter);
         }
 
 
